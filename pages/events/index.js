679e304a180d970451e19d0fa99eb79431886216
Aditya Patil <adityapatil@Adityas-MacBook-Air.local>
2023-03-07 16:12:39 +0530

    practice nextjs

---
+++ pages/events/index.js
@@ -0,0 +1,22 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+function AllEventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+  function findsEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findsEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
